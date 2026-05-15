746d4e1135181285e304626f16d360983221a171
Reymart A. Calicdan <reymart.calicdan@evsu.edu.ph>
2026-05-15 13:14:45 +0800

    fix wrong env variable in the config file for testing postgre version for dynamic version testing

--- tests/Pest.php
+++ tests/Pest.php
@@ -12,11 +12,11 @@ use function Hibla\await;
 function pgConfig(?PgSqlConfig $config = null): PgSqlConfig
 {
     return $config ?? new PgSqlConfig(
-        host: getenv('PG_HOST') ?: '127.0.0.1',
-        port: (int) (getenv('PG_PORT') ?: 5443),
-        username: getenv('PG_USER') ?: 'postgres',
-        password: getenv('PG_PASSWORD') ?: 'postgres',
-        database: getenv('PG_DB') ?: 'postgres',
+        host: $_ENV['POSTGRES_HOST'] ?? '127.0.0.1',
+        port: (int) ($_ENV['POSTGRES_PORT'] ?? 5443),
+        username: $_ENV['POSTGRES_USERNAME'] ?? 'postgres',
+        password: $_ENV['POSTGRES_PASSWORD'] ?? 'postgres',
+        database: $_ENV['POSTGRES_DATABASE'] ?? 'postgres',
         castPreparedTypes: false,
     );
 }
